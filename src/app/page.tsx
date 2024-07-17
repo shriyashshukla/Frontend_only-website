b6c17b94f173f733bc348df4fb05d115f0b7f8d3
Shriyash shukla <139564061+shriyashshukla@users.noreply.github.com>
2024-07-17 23:51:06 +0530

    chore: Update Navbar component and add navigation item to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 className="text-4x1 text-center">this is center</h1>
+    <h1 className="text-4xl text-center py-1">this is center</h1>
     </>
   );
 }
